a8d37dea0ae3c8592e78e612592e22cf375b37cc
victorusuwu <92773404+victorusuwu@users.noreply.github.com>
2021-11-23 12:07:05 +0700

    сохраняет программу в гитхаб

---
+++ сохраняет в гитхаб.py
@@ -0,0 +1,94 @@
+import pyautogui
+import time
+import keyboard
+
+
+a = input('введите имя файла который вы хотите загрузить на гитхаб')
+b = input('введите описание своей программе')
+pyautogui.moveTo(280, 750)
+pyautogui.click()
+time.sleep(1)
+pyautogui.press('ctrl')
+time.sleep(0.05)
+
+
+
+
+
+pyautogui.moveTo(1000, 55)
+pyautogui.click()
+pyautogui.press('del')
+time.sleep(1)
+pyautogui.click()
+keyboard.write('github.com')
+pyautogui.press('enter')
+pyautogui.moveTo(100, 260)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(815, 290)
+time.sleep(0.5)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(815, 350)
+time.sleep(0.5)
+pyautogui.click()
+pyautogui.moveTo(690, 500)
+time.sleep(0.5)
+pyautogui.click()
+time.sleep(0.5)
+keyboard.write(a)
+time.sleep(0.5)
+pyautogui.moveTo(280, 550)
+pyautogui.click()
+time.sleep(0.5)
+pyautogui.press('enter')
+time.sleep(0.5)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+pyautogui.moveTo(280, 320)
+pyautogui.click()
+keyboard.write(b)
+pyautogui.moveTo(200, 560)
+pyautogui.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
