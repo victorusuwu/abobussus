7a8af1842ec5a53c7502cc5f66d7b8fc3d5bd3bc
victorusuwu <92773404+victorusuwu@users.noreply.github.com>
2021-11-23 11:51:47 +0700

    аывавыаыва

--- запросв гугле.py
+++ запросв гугле.py
@@ -1,9 +1,10 @@
 import pyautogui
 import time
 import keyboard
-ask = input('какой запрос вы хотите узнать в браузере')
 
-number = input('введите номер вкладки')
+
+a = input('введите имя файла который вы хотите загрузить на гитхаб')
+b = input('введите описание своей программе')
 pyautogui.moveTo(280, 750)
 pyautogui.click()
 time.sleep(1)
@@ -11,32 +12,7 @@ pyautogui.press('ctrl')
 time.sleep(0.05)
 
 
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
 
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-
-pyautogui.hotkey('ctrl','t')
-time.sleep(0.05)
-pyautogui.hotkey('ctrl',number)
 
 
 pyautogui.moveTo(1000, 55)
@@ -44,5 +20,73 @@ pyautogui.click()
 pyautogui.press('del')
 time.sleep(1)
 pyautogui.click()
-keyboard.write(ask)
+keyboard.write('github.com')
 pyautogui.press('enter')
+pyautogui.moveTo(100, 260)
+time.sleep(1)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(815, 290)
+time.sleep(0.5)
+pyautogui.click()
+time.sleep(1)
+pyautogui.moveTo(815, 350)
+time.sleep(0.5)
+pyautogui.click()
+pyautogui.moveTo(690, 500)
+time.sleep(0.5)
+pyautogui.click()
+time.sleep(0.5)
+keyboard.write(a)
+time.sleep(0.5)
+pyautogui.moveTo(280, 550)
+pyautogui.click()
+time.sleep(0.5)
+pyautogui.press('enter')
+time.sleep(0.5)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+time.sleep(0.2)
+pyautogui.press('down')
+pyautogui.moveTo(280, 320)
+pyautogui.click()
+keyboard.write(b)
+pyautogui.moveTo(200, 540)
+
+
+
+
+
+
+
+
+
+
+
+
+
