b5ad8a94092ba5ebcf092382acfa6ed298d03bef
victorusuwu <92773404+victorusuwu@users.noreply.github.com>
2021-12-21 11:08:36 +0700

    Add files via upload

---
+++ цифры.py
@@ -0,0 +1,16 @@
+import keyboard
+while True:
+    if keyboard.is_pressed('1'):
+     print('1-маленькое число')
+    if keyboard.is_pressed('2'):
+     print('2точка 2 не выйдет никогда...')
+    if keyboard.is_pressed('3'):
+     print('ждем когда достроят гимназию номер 3')
+    if keyboard.is_pressed('4'):
+     print('а что если перед этой цифрой поставить букву а? а4 ГЫЫЫЛЕНТ КАБЯКОВ ГЫЫЫЛЕНТ ВЛАД БУМАГА')   
+    if keyboard.is_pressed('5'):
+     print('то что невозможно получить на русском')
+    if keyboard.is_pressed('6'):
+     print('в бравл старсе 6 легендарок леон(который мне за 4 года так и невыпал)ворон спайк сэнди(краш) амбер и мэг(NIGGER)')
+    if keyboard.is_pressed('7'):
+     print('азино 3 тапара')
