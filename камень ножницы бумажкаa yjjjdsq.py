654edbb96590d1178fb37e1a66e1f979394b2ec7
victorus <92773404+victorusuwu@users.noreply.github.com>
2022-04-12 10:56:07 +0700

    Add files via upload

---
+++ камень ножницы бумажкаa yjjjdsq.py
@@ -0,0 +1,22 @@
+import random
+print('Привет это игра камень ножницы бумага 1-камень 2-ножницы 3-бумага')
+bot=random.randint(1,3)
+while True:
+    ti =input('камень ножницы бумага')
+    ti = int(ti)
+    if ti == bot:
+        print('одинаково переигрываем')
+        bot=random.randint(1,3)
+        ti =input('камень ножницы бумага')
+    elif bot == 1 and ti == 3:
+        print('Вы победили')
+    elif bot == 2 and ti == 3:
+        print('Вы проиграли')
+    elif bot == 3 and ti == 2:
+        print('Вы победили ')
+    elif bot == 1 and ti == 2:
+        print('Вы проиграли')
+    elif bot == 2 and ti == 1:
+        print('Вы победили')
+    elif bot == 3 and ti == 1:
+        print('Вы проиграли')
